5d3d0dd01f2bb1284dc079f803dbc94586cc84ec
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-12-20 10:16:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f( float a){
+    float y=0;
+    y= ((a*a)* cos(a))+1;
+    return y;
+
 int main() {
-   return 0;
+    float a=0, b=0, x,err;
+    while((f(a)* f(b))>=0){
+        cout<<"inserire estremi"; cin>>a>>b;
+    }
+    x=(a+b)/2;
+    while ((f(x)!=0)and(err<(1/(exp(6))))){
+        x=(a+b)/2;
+        if ((f(x)*f(b))<0){
+            b=x;
+        }else{
+            a=x;
+        }
+        err= abs((b-a)/2);
+    }
+        cout<<x<<endl;
+    x=x*10000;
+    x=(int)x;
+    x=x/10000;
+    cout<< x;
+
+
+    return 0;
 }
+  
